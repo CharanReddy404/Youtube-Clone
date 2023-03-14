12dd239e49eb1021825ff118c1c3bee69972dfd1
CharanReddy404 <charanlucky6143@gmail.com>
2023-03-14 18:53:35 +0530

    sidebar

---
+++ src/components/SideBar.js
@@ -0,0 +1,25 @@
+const SideBar = () => {
+  return (
+    <div className=''>
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+      </ul>
+      <hr />
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+      </ul>
+      <hr />
+      <ul>
+        <li>Home</li>
+        <li>Shorts</li>
+        <li>Subscriptions</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
