9e705fdeedbb490cb8f3e5af5438e1f7920a64bf
CharanReddy404 <charanlucky6143@gmail.com>
2023-03-26 10:00:03 +0530

    watchpage

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
 const GOOGLE_API_KEY = 'AIzaSyCGbFPyt5KZ9gVQch_xYCgIiMY7Ov547dc';
 
-export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${GOOGLE_API_KEY}`;
+export const YOUTUBE_VIDEOS_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
